67415f216b3138243d740973a2b027b87b80940b
Tarun Kalva <tarunkalva14@gmail.com>
2025-12-24 22:12:23 -0800

    Add files via upload

---
+++ Stop-Watch/StopWatch.jsx
@@ -0,0 +1,58 @@
+import React, {useState, useEffect, useRef} from "react";
+
+function StopWatch(){
+
+    const[isRunning, setisRunning] = useState(false);
+    const[elapsedTime, setElapsedTime] = useState(0);
+    const intervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+    useEffect(() => {
+        if(isRunning){
+            intervalIdRef.current = setInterval(() => {
+                setElapsedTime(Date.now() - startTimeRef.current);
+            }, 10);
+        }
+
+        return() =>{
+            clearInterval(intervalIdRef.current);
+        }
+    }, [isRunning]);
+
+    function start(){
+        setisRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
+    }
+    function stop(){
+        setisRunning(false);
+    }
+    function reset(){
+        setElapsedTime(0);
+        setisRunning(false);
+    }
+    function formatTime(){
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+        let seconds = Math.floor(elapsedTime / (1000) % 60);
+        let milliseconds = Math.floor((elapsedTime) % 1000 / 10);
+
+        hours = String(hours).padStart(2, "0");
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+        milliseconds = String(milliseconds).padStart(2, "0");
+
+        return `${minutes}:${seconds}:${milliseconds}`;
+    }
+
+    return(
+        <div className="stop-watch">
+            <div className="display">{formatTime()}</div>
+            <div className="controls">
+                <button className = "start-button" onClick={start}>Start</button>
+                <button className = "stop-button" onClick={stop}>Stop</button>
+                <button className = "reset-button" onClick={reset}>Reset</button>
+            </div>
+        </div>
+    );
+}
+export default StopWatch

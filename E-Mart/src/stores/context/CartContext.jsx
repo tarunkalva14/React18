bd44c6faba5b4a557d8660df7bc6cba085b3d22b
Tarun Kalva <tarunkalva14@gmail.com>
2026-01-01 16:44:26 -0800

    Add files via upload

---
+++ E-Mart/src/stores/context/CartContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    setCartItems([...cartItems, product]);
+  };
+
+  const removeFromCart = (id) => {
+    setCartItems(cartItems.filter((item) => item.id !== id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
